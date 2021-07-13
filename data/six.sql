7b79757efb5c12d6b05a57c7add59e2e4c26db41
lh <295298896@qq.com>
2021-07-13 15:58:52 +0800

    修改

--- data/six.sql
+++ data/six.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50553
  File Encoding         : 65001
 
- Date: 12/07/2021 17:34:16
+ Date: 13/07/2021 15:58:41
 */
 
 SET NAMES utf8mb4;
@@ -78,18 +78,27 @@ CREATE TABLE `sys_role`  (
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user`  (
   `id` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `sex` tinyint(1) NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '登录名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'token',
+  `sex` tinyint(1) NULL DEFAULT NULL COMMENT '性别',
+  `company_id` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '公司id',
+  `department` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '部门id',
+  `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '头像',
+  `realname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '真实姓名',
+  `id_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '身份证',
+  `mobile` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '联系方式',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建人',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', 'admin', '9bcedfc16e4468552c8bb99900fff15362d60ca0bc471e5a008d89b27066b483', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWRtaW4iLCJleHAiOjE2MjYwODIxMzh9._EXyGXrhlEW69O3FN8EcN0e_3oQYAoOxG8NhiKYvoyA', 1);
-INSERT INTO `sys_user` VALUES ('2', 'ss', '123', NULL, 0);
-INSERT INTO `sys_user` VALUES ('3', 'dd', '124', NULL, 1);
+INSERT INTO `sys_user` VALUES ('1', 'admin', '9bcedfc16e4468552c8bb99900fff15362d60ca0bc471e5a008d89b27066b483', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYWRtaW4iLCJleHAiOjE2MjYyNDY5MjZ9.Dr11GtnMPqmBP24pzociabiqvHckJPuDPDfq-Ed-YNI', 1, '1', '1', NULL, '超级管理员', '1223456', '123457', '2021-07-13 10:53:14', '1');
+INSERT INTO `sys_user` VALUES ('2', 'ss', '123', NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `sys_user` VALUES ('3', 'dd', '124', NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `sys_user` VALUES ('c5d4d1d3c50f4673875beec00b527ed2', 'sckr', '9bcedfc16e4468552c8bb99900fff15362d60ca0bc471e5a008d89b27066b483', NULL, 0, '1', '1', NULL, '自己的2221', '123456789', '123456699', NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
